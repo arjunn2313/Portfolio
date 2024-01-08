8f461a6fad78d826fbaeceeaf5c2768a18d0643c
arjunn2313 <arjunnks123@gmail.com>
2024-01-08 10:05:32 +0530

    project page completed

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,22 +4,59 @@ export default function Project() {
   return (
     <div className="text-white">
       <h2 className="text-center pt-2 text-2xl font-nunito">My Projects</h2>
-      <div className="flex flex-col gap-5 mt-3 overflow-hidden">
-        <div className="w-11/12 h-44 mx-auto bg-gray-900 flex">
-          <div className="w-1/2 h-full flex flex-col justify-center items-center gap-2"> 
-            <h2 className="text-center">Scope India</h2>
-            <p className="text-sm text-gray-300 text-start px-3">
+      <div className="flex flex-col gap-5 mt-3 md:flex-row md:flex-wrap md:gap-4">
+        {/* PROJECT 1 */}
+        <div className="w-11/12 h-48 mx-auto bg-gray-900 md:w-1/3 flex md:h-64">
+          <div className="w-full  h-full flex flex-col justify-center items-center gap-2 md:gap-7 ">
+            <h2 className="text-center text-2xl">Scope India</h2>
+            <p className="text-md text-gray-300 text-start px-3 font-serif">
               Aim to provide a platform for students to access various courses
               offered by the institute, register, validate their accounts, and
               enroll into the course.
             </p>
+            <div className="flex gap-3">
+              <span className="px-3 bg-gray-500 rounded-full">React</span>
+              <span className="px-3 bg-gray-500 rounded-full">MongoDB</span>
+              <span className="px-3 bg-gray-500 rounded-full">Express</span>
+              <span className="px-3 bg-gray-500 rounded-full">Redux</span>
+            </div>
           </div>
-          <div  className="flex items-center justify-end w-1/2 overflow-hidden">
-                <img src="assets\Screenshot (13).png" className="w-4/5 h-3/4"/>
+           
+        </div>
+        {/* PROJECT 2 */}
+        <div className="w-11/12 h-48 mx-auto bg-gray-800 md:w-1/3 md:h-64">
+          <div className="w-full   h-full flex flex-col justify-center items-center gap-2 md:gap-7">
+            <h2 className="text-center text-2xl">Hospital Manager</h2>
+            <p className="text-md text-gray-300 text-start px-3 font-serif">
+              Aim to provide a platform for students to access various courses
+              offered by the institute, register, validate their accounts, and
+              enroll into the course.
+            </p>
+            <div className="flex gap-3">
+              <span className="px-3 bg-gray-500 rounded-full">React</span>
+              <span className="px-3 bg-gray-500 rounded-full">MongoDB</span>
+              <span className="px-3 bg-gray-500 rounded-full">Express</span>
+              <span className="px-3 bg-gray-500 rounded-full">Redux</span>
+            </div>
           </div>
         </div>
-        <div className="w-11/12 h-44 mx-auto bg-gray-800">uj</div>
-        <div className="w-11/12 h-44 mx-auto bg-gray-900">uj</div>
+        {/* PROJECt 3 */}
+        <div className="w-11/12 h-44 mx-auto bg-gray-900 md:h-64 md:w-1/3">
+        <div className="w-full   h-full flex flex-col justify-center items-center gap-2 md:gap-7"> 
+            <h2 className="text-center text-2xl">Booking.Com</h2>
+            <p className="text-md text-gray-300 text-start px-3 font-serif">
+              Aim to provide a platform for students to access various courses
+              offered by the institute, register, validate their accounts, and
+              enroll into the course.
+            </p>
+            <div className="flex gap-3">
+            <span className="px-3 bg-gray-500 rounded-full">React</span>
+            <span className="px-3 bg-gray-500 rounded-full">MongoDB</span>
+            <span className="px-3 bg-gray-500 rounded-full">Express</span>
+            <span className="px-3 bg-gray-500 rounded-full">Redux</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
