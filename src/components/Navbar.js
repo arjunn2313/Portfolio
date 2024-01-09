aa8e2b85fb1b5a1c9ddf598220e938b0150c4197
Arjun KS <145977909+arjunn2313@users.noreply.github.com>
2024-01-09 09:22:07 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,8 +16,8 @@ export default function Navbar() {
               </ul> */}
                <div className='flex gap-5'>
               <a href='https://github.com/arjunn2313'><FaGithub className='text-2xl'/></a> 
-              <a href=''><FaLinkedin className='text-2xl'/></a> 
-              <a href=''><FaInstagram className='text-2xl'/></a> 
+              <a href='https://www.linkedin.com/in/arjun-k-s-86aa59222'><FaLinkedin className='text-2xl'/></a> 
+              <a href='https://www.instagram.com/ar_j__un/'><FaInstagram className='text-2xl'/></a> 
               <a href=''><FaFacebook className='text-2xl'/></a> 
                </div>
             </div>
